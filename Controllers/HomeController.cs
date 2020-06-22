fc668104aaed9b6b2eeaf90713ceec331c089f93
Jarek700 <a70t9zz@mmm.com>
2020-06-22 15:00:44 +0200

    zxc

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using BlogNew1.Context;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,17 +14,18 @@ namespace BlogNew1.Models
         
         public ActionResult Index()
         {
-
-          return View();
+            ArtykulyContext db = new ArtykulyContext();
+            var artykul = db.Artykuly;
+            return View();
 
         }
        // [Authorize(Roles ="Admin")]                   //autoryzacja na później
         [HttpPost]
         public ActionResult DodajArtykul(Artykuly artykuly)
         {
-            if (!ModelState.IsValid)                        // walidacja z negacją
-                return View("DodajArtykul", artykuly);
-            else
+            //if (!ModelState.IsValid)                        // walidacja z negacją
+            //    return View("DodajArtykul", artykuly);
+            //else
             {
                 ArtykulyContext db = new ArtykulyContext();
                 db.Artykuly.Add(artykuly);
@@ -36,5 +38,29 @@ namespace BlogNew1.Models
         {
                 return View();
         }
+
+
+        //-------- Dostęp do bazy przy urzciu ADO.NET - bezpośrednie zapytania do bazy danych
+        //string ArtykulyConnectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = ArtykulyConnectionString; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+        //string query = "Select Tytul, TekstArtykulu from dbo.Artykulies where id = @id";
+
+        //int id = 2;
+
+        //using (SqlConnection connection = new SqlConnection(ArtykulyConnectionString))
+        //{
+        //    SqlCommand command = new SqlCommand(query, connection);
+        //    command.Parameters.AddWithValue("@id", id);
+        //    connection.Open();
+        //    SqlDataReader reader = command.ExecuteReader();
+
+        //    reader.Read();
+
+        //    var Tytul = reader[0];
+        //    var TekstArtykulu = reader[1];
+
+        //    ViewBag.Tytul = Tytul;
+        //    ViewBag.TekstArtykulu = TekstArtykulu;
+        //}
     }
 }
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace BlogNew1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BlogNew1.Context.ArtykulyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "BlogNew1.Context.ArtykulyContext";
+        }
+
+        protected override void Seed(BlogNew1.Context.ArtykulyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
